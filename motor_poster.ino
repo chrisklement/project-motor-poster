4266ea99790c5467fcc66a30902d5ac352de437f
chrisklement <hello@chrisklement.com>
2015-09-28 15:50:34 +0200

    code update

--- motor_poster.ino
+++ motor_poster.ino
@@ -1,13 +1,13 @@
 // Slider 1
-int motor1A = 2;
-int motor1B = 4;
+int motor1A = 4;
+int motor1B = 2;
 int motor1SpeedPin = 3;
 int motor1poti = A0;
 int motor1potivalue = 0;
 
 // Slider 2
-int motor2A = 8;
-int motor2B = 7;
+int motor2A = 7;
+int motor2B = 8;
 int motor2SpeedPin = 9;
 int motor2poti = A1;
 int motor2potivalue = 0;
@@ -24,18 +24,25 @@ int sensor = A3;
 int sensorValue = 0;
 boolean isInAction = false;
 
+// How much the Fader1 should move
 int counterFader1 = 0; // counter for movement
 int maxcountFader1 = 7; // maximum movemtents
 
+// How much the Fader2 should move
 int counterFader2 = 0; // counter for movement
 int maxcountFader2 = 7; // maximum movemtents
 
-long randomNumber;
+// Random and match settings
+long randomNumber1;
+long randomNumber2;
 
-int match = 0;
+int matchSlider1 = 0;
+int matchSlider2 = 0;
 boolean restart = false;
 
+// Timer settings
 long previousMillis = 0;
+long previousMillisSensor = 0;
 
 long interval = 5000;
 long intervalRestart = 8000;
@@ -59,18 +66,17 @@ void loop() {
   
   unsigned long currentMillis = millis();
   
-  sensorValue = analogRead(sensor);
-  Serial.println(isInAction);
+  //Serial.println(isInAction);
   
-
   if(isInAction == false) {
     sensorValue = analogRead(sensor);
+    sensorValue = constrain(sensorValue, 200, 500);
   }
   
   if(motor3potivalue > 920) {
     if((currentMillis - previousMillis > intervalRestart)) {
       if(currentMillis - previousMillis > interval) {
-        if(sensorValue > 300) {
+        if(sensorValue > 250) {
           isInAction = true;
         }
       }
@@ -109,7 +115,7 @@ void movetheFader() {
   if (counterFader1 < maxcountFader1) {
   
     // Slider 1 Direction one
-    if (motor1potivalue > 750) {
+    if (motor1potivalue > 730) {
       digitalWrite(motor1A, HIGH);  // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor1B, LOW);   // motor direction two pin switched to LOW  (GND)
       analogWrite(motor1SpeedPin, 160);  // and write it as speed to the speed pin
@@ -140,7 +146,7 @@ void movetheFader() {
   if (counterFader2 < maxcountFader2) {
     
     // Slider 2 Direction one
-    if (motor2potivalue > 750) {
+    if (motor2potivalue > 715) {
       digitalWrite(motor2A, HIGH);  // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor2B, LOW);   // motor direction two pin switched to LOW  (GND)
       analogWrite(motor2SpeedPin, 180);  // and write it as speed to the speed pin
@@ -150,7 +156,7 @@ void movetheFader() {
     } 
       
     // Slider 2 Direction two
-    if (motor2potivalue < 250) {
+    if (motor2potivalue < 270) {
       digitalWrite(motor2A, LOW);   // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor2B, HIGH);  // motor direction two pin switched to LOW  (GND)
       analogWrite(motor2SpeedPin, 180);  // and write it as speed to the speed pin
@@ -172,20 +178,33 @@ void movetheFader() {
       digitalWrite(motor1A, LOW); // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor1B, LOW); // motor direction two pin switched to LOW  (GND)
       
-      if(randomNumber == 1){
-        match = 1; 
+      if(randomNumber1 == 1){
+        matchSlider1 = 1; 
       } 
   
       else {
-        match = 2; 
+        matchSlider1 = 2; 
+      }
+      
+      if(randomNumber2 == 1){
+        matchSlider2 = 1; 
+      } 
+  
+      else {
+        matchSlider2 = 2; 
       }
       
       isInAction = false;
       
       // Random counter
-      randomNumber = random(2);
+      randomNumber1 = random(2);
+      maxcountFader1 = 7;
+      maxcountFader1 = maxcountFader1 + randomNumber1;
+      
+      // Random counter
+      randomNumber2 = random(2);
       maxcountFader2 = 7;
-      maxcountFader2 = maxcountFader2 + randomNumber;
+      maxcountFader2 = maxcountFader2 + randomNumber2;
 
       counterFader1 = 0;
       counterFader2 = 0;
@@ -194,8 +213,8 @@ void movetheFader() {
 }
 
 void faderMatchMove() {
-  if(match == 1){
-    if (motor3potivalue > 920) {
+  if(matchSlider1 != matchSlider2){
+    if (motor3potivalue > 880) {
       digitalWrite(motor3A, HIGH);  // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor3B, LOW);  // motor direction one pin switched to HIGH (+5V)
       analogWrite(motor3SpeedPin, 200);  // and write it as speed to the speed pin
@@ -209,14 +228,14 @@ void faderMatchMove() {
     }
   }
   
-  if(match == 2){
-    if (motor3potivalue > 920) {
+  if((matchSlider1 == 1 && matchSlider2 == 1) || (matchSlider1 == 2 && matchSlider2 == 2)){
+    if (motor3potivalue > 880) {
       digitalWrite(motor3A, HIGH);  // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor3B, LOW);  // motor direction one pin switched to HIGH (+5V)
       analogWrite(motor3SpeedPin, 200);  // and write it as speed to the speed pin
     }
     
-    if (motor3potivalue < 530) {
+    if (motor3potivalue < 550) {
       digitalWrite(motor3A, LOW);  // motor direction one pin switched to HIGH (+5V)
       digitalWrite(motor3B, LOW);  // motor direction one pin switched to HIGH (+5V)
       analogWrite(motor3SpeedPin, 0);  // and write it as speed to the speed pin
@@ -231,11 +250,12 @@ void faderRestart() {
       digitalWrite(motor3B, HIGH);  // motor direction one pin switched to HIGH (+5V)
       analogWrite(motor3SpeedPin, 200);  // and write it as speed to the speed pin
      
-      if (motor3potivalue > 920) {
+      if (motor3potivalue > 880) {
         digitalWrite(motor3A, LOW);  // motor direction one pin switched to HIGH (+5V)
         digitalWrite(motor3B, LOW);  // motor direction one pin switched to HIGH (+5V)
         analogWrite(motor3SpeedPin, 0);  // and write it as speed to the speed pin
-        match = 0;
+        matchSlider1 = 0;
+        matchSlider2 = 0;
         restart = false;
       }
     }
@@ -246,18 +266,18 @@ void calibrateFader() {
   //Send fader to the top and read max position
   digitalWrite(motor1A, HIGH);
   digitalWrite(motor2A, HIGH);
-  //digitalWrite(motor3A, HIGH);
+  digitalWrite(motor3A, HIGH);
   delay(400);
   digitalWrite(motor1A, LOW);
   digitalWrite(motor2A, LOW);
-  //digitalWrite(motor3A, LOW); 
+  digitalWrite(motor3A, LOW); 
     
   //Send fader to the bottom and read max position
   digitalWrite(motor1B, HIGH);
   digitalWrite(motor2B, HIGH);
-  //digitalWrite(motor3B, HIGH);
+  digitalWrite(motor3B, HIGH);
   delay(400);
   digitalWrite(motor1B, LOW);
   digitalWrite(motor2B, LOW);
-  //digitalWrite(motor3B, LOW);
+  digitalWrite(motor3B, LOW);
 }
